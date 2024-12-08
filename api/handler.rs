002259b4335995f26455b3d1a2901225131b2087
beetlepig <devpig@icloud.com>
2024-12-08 09:29:45 -0500

    add rust endpoint

--- api/handler.rs
+++ api/handler.rs
@@ -1,6 +1,7 @@
+use std::path::PathBuf;
 use serde_json::json;
 use vercel_runtime::{run, Body, Error, Request, Response, StatusCode};
-use kalosm::language::{Chat, Llama, LlamaSource, TextStream};
+use kalosm::language::{Chat, ChatMarkers, FileSource, Llama, LlamaSource, TextStream};
 
 
 #[tokio::main]
@@ -10,16 +11,33 @@ async fn main() -> Result<(), Error> {
 
 pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
     let model = Llama::builder()
-        .with_source(LlamaSource::llama_3_2_3b_chat())
+        .with_source(
+            LlamaSource::new(
+                FileSource::Local(PathBuf::from("./tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf")),
+                FileSource::Local(PathBuf::from("./tokenizer.json")),
+            )
+                .with_chat_markers(ChatMarkers {
+                    system_prompt_marker: "<|system|>\n",
+                    assistant_marker: "<|user|>\n",
+                    user_marker: "<|assistant|>\n",
+                    end_system_prompt_marker: "</s>",
+                    end_user_marker: "</s>",
+                    end_assistant_marker: "</s>",
+                })
+                .with_group_query_attention(4),
+        )
         .build()
         .await
         .unwrap();
 
     let mut chat = Chat::builder(model)
-        .with_system_prompt("The assistant will act like a pirate")
+        .with_system_prompt("You are an AI assistant created by Hominid Interactive")
         .build();
 
-    let response = chat.add_message("hola").all_text().await;
+    let response = chat
+        .add_message("What is your name and who is your creator?")
+        .all_text()
+        .await;
 
     Ok(Response::builder()
         .status(StatusCode::OK)
