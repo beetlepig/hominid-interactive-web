9fc90853105f3146bebb496687a6f396bbe99342
beetlepig <devpig@icloud.com>
2024-12-08 12:31:22 -0500

    add rust endpoint

--- api/handler.rs
+++ api/handler.rs
@@ -13,6 +13,8 @@ pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
     let current_dir = env::current_dir().unwrap();
 
     let model_path = current_dir.join("util/tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf");
+
+    panic!(model_path.metadata().unwrap());
     let tokenizer_path = current_dir.join("util/tokenizer.json");
 
     let model = Llama::builder()
