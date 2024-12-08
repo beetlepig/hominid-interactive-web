5adcdbe1ffdbd9f8fca4afb28b0c7e3001300bc4
beetlepig <devpig@icloud.com>
2024-12-07 20:50:22 -0500

    add rust endpoint

--- api/handler.rs
+++ api/handler.rs
@@ -1,6 +1,7 @@
 use serde_json::json;
 use vercel_runtime::{run, Body, Error, Request, Response, StatusCode};
-use kalosm::language::*;
+use kalosm::language::{Chat, Llama, LlamaSource, TextStream};
+
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -8,22 +9,24 @@ async fn main() -> Result<(), Error> {
 }
 
 pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
-    let model = Llama::new_chat().await.expect("kk");
+    let model = Llama::builder()
+        .with_source(LlamaSource::llama_3_2_3b_chat())
+        .build()
+        .await
+        .unwrap();
 
-    // New code
     let mut chat = Chat::builder(model)
         .with_system_prompt("The assistant will act like a pirate")
         .build();
 
-
-    let xx = chat.add_message("hola").all_text().await;
+    let response = chat.add_message("hola").all_text().await;
 
     Ok(Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "application/json")
         .body(
             json!({
-              "message": xx
+              "message": response
             })
                 .to_string()
                 .into(),
