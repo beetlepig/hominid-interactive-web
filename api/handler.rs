9d3125022fcbbaff3f989a8b81410683ab3ed166
beetlepig <devpig@icloud.com>
2024-12-08 11:01:39 -0500

    add rust endpoint

--- api/handler.rs
+++ api/handler.rs
@@ -12,8 +12,6 @@ async fn main() -> Result<(), Error> {
 pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
     let current_dir = env::current_dir().unwrap();
 
-    panic!("{:?}", current_dir);
-
     let model_path = current_dir.join("tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf");
     let tokenizer_path = current_dir.join("tokenizer.json");
 
