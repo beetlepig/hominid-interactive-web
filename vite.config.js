22a4c5eaf958d177e025d3c93c66ed0b1ec1854b
beetlepig <devpig@icloud.com>
2025-09-01 18:03:07 -0500

    Optimizations

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { threeMinifier } from '@yushijinhun/three-minifier-rollup';
 import Icons from 'unplugin-icons/vite';
 import { defineConfig } from 'vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
 
 export default defineConfig({
 	plugins: [
+		{ ...threeMinifier(), enforce: 'pre' },
 		sveltekit(),
 		devtoolsJson(),
 		Icons({
