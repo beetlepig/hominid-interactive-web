8f3c204317fea20a7109c3ed8ad0d9cd92885a46
beetlepig <devpig@icloud.com>
2025-09-04 18:16:39 -0500

    Add paraglide translations

--- src/lib/constans/page-sections.js
+++ src/lib/constans/page-sections.js
@@ -1,10 +1,12 @@
+import { m } from '$lib/paraglide/messages.js';
+
 const sections = {
-	home: { name: 'Home', href: '#home', id: 'home' },
-	aboutMe: { name: 'About Me', href: '#about-me', id: 'about-me' },
-	skills: { name: 'Skills', href: '#skills', id: 'skills' },
-	projects: { name: 'Projects', href: '#projects', id: 'projects' },
-	chat: { name: 'Chat', href: '#chat', id: 'chat' },
-	contact: { name: 'Contact', href: '#contact', id: 'contact' }
+	home: { name: m.menu_home(), href: '#home', id: 'home' },
+	aboutMe: { name: m.menu_about_me(), href: '#about-me', id: 'about-me' },
+	skills: { name: m.menu_skills(), href: '#skills', id: 'skills' },
+	projects: { name: m.menu_projects(), href: '#projects', id: 'projects' },
+	chat: { name: m.menu_chat(), href: '#chat', id: 'chat' },
+	contact: { name: m.menu_contact(), href: '#contact', id: 'contact' }
 };
 
 export { sections };
