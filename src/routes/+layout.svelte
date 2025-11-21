dc987f3c5ef018f0a501cc083fef1559fa7ae888
beetlepig <devpig@icloud.com>
2025-11-21 13:34:56 -0500

    Fix SSG

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
+	import { page } from '$app/state';
 	import { Menu } from '$components/ui/organisms/menu';
 	import faviconSVG from '$lib/assets/favicon.svg';
+	import { locales, localizeHref } from '$lib/paraglide/runtime';
 	import '../app.css';
 	import '@fontsource-variable/oswald';
 	import '@fontsource-variable/raleway';
@@ -11,6 +13,12 @@
 <svelte:head>
 	<link rel="icon" href={faviconSVG} type="image/svg+xml" />
 </svelte:head>
+
+<div style="display:none">
+	{#each locales as locale}
+		<a href={localizeHref(page.url.pathname, { locale })}>{locale}</a>
+	{/each}
+</div>
 
 <Menu />
 
