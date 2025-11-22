901865ca0bbf466374520927bdcf649a41f44f46
beetlepig <devpig@icloud.com>
2025-11-22 01:00:59 -0500

    Refactor carousel

--- src/routes/home/components/ui/organisms/projects/ProjectElement.svelte
+++ src/routes/home/components/ui/organisms/projects/ProjectElement.svelte
@@ -56,11 +56,8 @@
 	});
 </script>
 
-<li
-	bind:this={projectsRef}
-	class="w-[82vw] snap-center rounded-3xl bg-background p-8 shadow-sm md:w-xl md:p-20"
->
-	<div class="flex h-full flex-col justify-between gap-6">
+<li class="w-[82vw] snap-center rounded-3xl bg-background p-8 shadow-sm md:w-xl md:p-20">
+	<div bind:this={projectsRef} class="flex h-full flex-col justify-between gap-6">
 		<div>
 			<h3 class="font-raleway text-3xl font-bold">
 				{@render projectName()}
